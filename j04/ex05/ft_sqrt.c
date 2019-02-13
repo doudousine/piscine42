22fcb6fd2195ac3b918cd6af390fd2d3d7afdd07
Djibril SY <djsy@e3r6p9.42.fr>
2019-02-13 23:42:26 +0100

    day 4 retry 2

--- j04/ex05/ft_sqrt.c
+++ j04/ex05/ft_sqrt.c
@@ -6,9 +6,20 @@
 /*   By: djsy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 14:40:46 by djsy              #+#    #+#             */
-/*   Updated: 2019/02/13 21:08:02 by djsy             ###   ########.fr       */
+/*   Updated: 2019/02/13 23:40:49 by djsy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		checkentier(float x)
+{
+	int result;
+	
+	result = x - (int)x;
+	if (result != 0)
+		return (0);
+	else 
+		return (1);
+}
 
 int		ft_sqrt(int nb)
 {
@@ -26,5 +37,8 @@ int		ft_sqrt(int nb)
 		x = (x + y) / 2;
 		y = nb / x;
 	}
-	return (x);
+	if (checkentier(x)
+		return (x);
+	else
+		return (0);
 }
