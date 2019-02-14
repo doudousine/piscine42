3ad9e8e35144ae232d0529958a9ac7d87f036d7d
Djibril SY <djsy@e3r7p16.42.fr>
2019-02-14 14:50:26 +0100

    last day retry

--- j04/ex04/ft_fibonacci.c
+++ j04/ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: djsy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 16:49:01 by djsy              #+#    #+#             */
-/*   Updated: 2019/02/13 21:10:06 by djsy             ###   ########.fr       */
+/*   Updated: 2019/02/14 13:03:38 by djsy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@ int		ft_fibonacci(int index)
 {
 	if (index == 0)
 		return (0);
-	else if (index < 0)
+	if (index < 0)
 		return (-1);
-	else
-		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
--- j04/ex05/ft_sqrt.c
+++ j04/ex05/ft_sqrt.c
@@ -6,19 +6,19 @@
 /*   By: djsy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 14:40:46 by djsy              #+#    #+#             */
-/*   Updated: 2019/02/13 23:40:49 by djsy             ###   ########.fr       */
+/*   Updated: 2019/02/14 14:49:17 by djsy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		checkentier(float x)
+int		checkentier(float nb)
 {
-	int result;
-	
-	result = x - (int)x;
-	if (result != 0)
-		return (0);
-	else 
+	float result;
+
+	result = nb - (int)nb;
+	if (result == 0)
 		return (1);
+	else
+		return (0);
 }
 
 int		ft_sqrt(int nb)
@@ -37,7 +37,7 @@ int		ft_sqrt(int nb)
 		x = (x + y) / 2;
 		y = nb / x;
 	}
-	if (checkentier(x)
+	if (checkentier(x))
 		return (x);
 	else
 		return (0);
