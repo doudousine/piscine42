c114c2d6442f92aac525bc85bf5413eba3cdeafe
Djibril SY <djsy@e3r3p16.42.fr>
2019-02-19 17:29:45 +0100

    minor fix

---
+++ j06/ex05/ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djsy <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/14 22:00:32 by djsy              #+#    #+#             */
+/*   Updated: 2019/02/14 23:15:02 by djsy             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+	int strlen;
+
+	i = 0;
+	j = 0;
+	strlen = 0;
+	while (to_find[strlen])
+		strlen++;
+	if (strlen == 0)
+		return (0);
+	while (str[i])
+	{
+		while (str[i + j] == to_find[j])
+		{
+			if (j == strlen - 1)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
