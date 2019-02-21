40165a93ea521ba627783c5567dfb1bb04bbffe5
Djibril SY <djsy@e3r4p16.42.fr>
2019-02-21 11:42:20 +0100

    minor fix

--- j06/ex00/libft_creator.sh
+++ j06/ex00/libft_creator.sh
@@ -1,4 +1,4 @@
 #!/bin/nash
 gcc -c -Wall -Werror -Wextra ft_putchar.c ft_putstr.c ft_strlen.c ft_swap.c ft_strcmp.c
-ar rc libft.a ft_putchar.o ft_putstr.o ft_strlen.o ft_swap.c ft_strcmp.o
+ar rc libft.a ft_putchar.o ft_putstr.o ft_strlen.o ft_swap.o ft_strcmp.o
 
--- j06/ex04/ft_sort_params.c
+++ j06/ex04/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: djsy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 05:06:56 by djsy              #+#    #+#             */
-/*   Updated: 2019/02/20 18:20:02 by djsy             ###   ########.fr       */
+/*   Updated: 2019/02/21 11:40:42 by djsy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,24 @@ int		ft_strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] && s2[i])
-	{
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-		if (s1[i] == s2[i])
-			return (s1[i] - s2[i]);
-	}
-	return (0);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
 
 int		main(int argc, char **argv)
 {
 	int		i;
-	char	*recipient[argc];
+	char	*recipient;
 
-	i = 1;
+	i = 0;
 	while (i < argc - 1)
 	{
-		if (ft_strcmp(argv[i], argv[i + 1]) < 0)
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
 		{
-			recipient[i] = argv[i];
+			recipient = argv[i];
 			argv[i] = argv[i + 1];
-			argv[i + 1] = recipient[i];
+			argv[i + 1] = recipient;
 			i = 0;
 		}
 		i++;
