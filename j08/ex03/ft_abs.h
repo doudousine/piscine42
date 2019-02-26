c2a8f96287083e5c6126240829fe6f42408f0ad3
Djibril SY <djsy@e3r2p8.42.fr>
2019-02-26 16:28:10 +0100

    conitnue day 8

--- j08/ex03/ft_abs.h
+++ j08/ex03/ft_abs.h
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: djsy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/26 11:25:17 by djsy              #+#    #+#             */
-/*   Updated: 2019/02/26 16:07:26 by djsy             ###   ########.fr       */
+/*   Created: 2019/02/26 15:56:00 by djsy              #+#    #+#             */
+/*   Updated: 2019/02/26 16:06:50 by djsy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef FT_H
-# define FT_H
-
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_szap(int *a, int *b);
-int		ft_strcmp(char *s1, char *s2);
-int		ft_strlen(char *str);
-
+#ifndef FT_ABS_H
+# define ABS(Value) (Value < 0) ? -Value : Value
 #endif
